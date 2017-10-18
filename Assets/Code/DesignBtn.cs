da66e84bec89b154bfd927f8fa0f5a2debbea0e4
unknown <mariusz.lewandowski13@gmail.com>
2017-10-18 22:02:45 +0200

    working on second layout

---
+++ Assets/Code/DesignBtn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DesignBtn : MonoBehaviour {
+
+    void Start()
+    {
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(LoadSecondScene);
+    }
+
+    void LoadSecondScene()
+    {
+        SceneManager.LoadScene(ApplicationData.designScene);
+    }
+}
