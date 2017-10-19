23797fc18c46ba8f3e40124de3e01afa16e18efb
unknown <mariusz.lewandowski13@gmail.com>
2017-10-19 13:12:26 +0200

    Adding new objects to scene

---
+++ Assets/Code/SpawningManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningManager : MonoBehaviour {
+
+    public GameObject SpawnObject(GameObject objToSpawn, Vector3 position, Quaternion rotation = new Quaternion())
+    {
+       return  Instantiate(objToSpawn, position, rotation);
+    }
+}
